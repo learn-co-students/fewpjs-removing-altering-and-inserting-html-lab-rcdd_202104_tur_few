813c103eeae5ae155a18200664f2739d8dde981f
awadbilal <awadbilal99@gmail.com>
2021-04-21 04:45:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+let main = document.getElementById('main');
+main.remove();
+
+let element = document.querySelector('body');
+
+for(let i = 0; i < 3; i++){
+        let newHeader = document.createElement('h1');
+        newHeader.setAttribute('id', 'victory');
+        if (i == 2){
+                newHeader.innerHTML = ("Belal is the champion").toString();
+        }
+        element.appendChild(newHeader);
+}
