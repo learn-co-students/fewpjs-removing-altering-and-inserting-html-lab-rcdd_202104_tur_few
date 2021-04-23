af67ef724e04dbdbb2aa1fd56870c708edec08d2
DKay-code <derya.kaygisiz@outlook.com>
2021-04-24 00:16:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-// Write your code here!
-let element = document.createElement('div')
+const main = document.querySelector("main#main");
+ main.remove();
 
-document.body.appendChild(element)
+ const newHeader = document.createElement('h1');
+ newHeader.setAttribute("id", "victory");
 
-let ul = document.createElement('ul')
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
- 
-element.appendChild(ul)
-
-let element = document.querySelector("p#greeting");
-element.innerHTML = 'Hello, DOM!'
-
-let header = document.getElementById("div#header");
-header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
-
-content = someTextArea.value
-node.innerHTML = `Hi, ${content}!`
+ document.body.appendChild(newHeader);
+ newHeader.innerHTML = "YOUR-NAME is the champion";
