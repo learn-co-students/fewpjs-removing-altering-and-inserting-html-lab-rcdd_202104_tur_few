38caddbae2f9e3d166a6e4f789be1722d96ae50e
Moulham Hallak <mou990@gmail.com>
2021-05-11 22:31:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.querySelector('main#main');
+element.remove('');
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "Hello is the champion";
+document.body.appendChild(newHeader);
