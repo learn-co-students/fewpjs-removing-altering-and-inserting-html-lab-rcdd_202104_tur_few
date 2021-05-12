f2cc392e11855ea699780ca37f310caefb80e7f6
iseluin <kutay.kagan.ozenn@gmail.com>
2021-05-12 09:54:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let main = document.getElementById(main);
+main.remove();
+
+const h1 = document.createElement("h1");
+h1.id = "victory";
+h1.innerHTML("We are the champions");
